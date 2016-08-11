684dcae24ef9b7bfea02a6f3387a14f96825ddc0
dongwin22 <dongwin21@126.com>
2016-08-11 11:12:15 +0800

    调试

--- app/src/main/java/com/example/dllo/keeptest/MainActivity.java
+++ app/src/main/java/com/example/dllo/keeptest/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 6666
+        //5555555555
+        //9
     }
 }
